8928c6df1f4c80464acf042bdb92b9164e1285d3
unn04012 <unn04012@naver.com>
2021-01-26 05:17:56 +0900

    파일 처리 기능 추가
    이미지 파일 저장 기능 추가

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ passportConfig();
 app.set('port', process.env.PORT || 8000);
 app.set('view engine', 'ejs');
 
-sequelize.sync({ force: true })
+sequelize.sync({ force: false })
   .then(() => {
     console.log('데이터베이스 연결 성공');
   })
--- routes/mentor.js
+++ routes/mentor.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const Mentor = require('../models/mentor');
+const fs = require('fs');
+const multer = require('multer');
+const path = require('path');
+
+// upload 폴더 생성
+try{
+    fs.readdirSync('uploads');        
+}catch(error){
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    fs.mkdirSync('uploads');
+}
+
+// 파일 관리 
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req,file, done){
+            done(null, 'uploads/');
+        },
+        filename(req, file, done){
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits : {fileSize : 5 * 1024 * 1024},
+});
 
 router.get('/', async (req, res, next) => {
     try {
@@ -11,7 +36,7 @@ router.get('/', async (req, res, next) => {
     }
 });
 
-router.post('/', async(req, res, next) => {    
+router.post('/',  upload.single('image'), async(req, res, next) => {    
     const {username, gender, firm, department, career, field, email, intro, path} = req.body;
     try{
         const mentor = await Mentor.findOne({
@@ -20,12 +45,12 @@ router.post('/', async(req, res, next) => {
             }
         });
         if(mentor){
-            return res.send("<script>alert('이미 등록된 회원입니다')</script>");
+            res.render('home/mentor', {flag : false})                        
         }
         await Mentor.create({
             username, gender, firm, department, career, field, email, intro, path
-        });        
-        return res.redirect('/home/');
+        });                
+        res.render('home/mentor', {flag : true})                        
     }catch(err){
         console.error(err);
         return next(err);
