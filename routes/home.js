4e51d363520c09ba5d3289fd906c39331704edfe
IOUIOU50 <wikis2101@naver.com>
2021-01-27 20:44:42 +0900

    [직업소개]개요 및 직종 세부설명 페이지

--- routes/home.js
+++ routes/home.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const User = require('../models/user');
 
+const fs = require('fs');
+
 const router = express.Router();
 
 //session 유지를 위한 > passport module
@@ -66,6 +68,39 @@ router.post('/login', isNotLoggedIn, (req, res, next) => {
 router.get('/auth', isNotLoggedIn, async(req,res,next)=>{
   try{
     res.render('home/auth');
+  }catch(err){
+    console.error(err);
+    next(err);
+  }
+});
+
+//직업소개페이지 개요
+router.get('/job_seeking/intro', async(req, res, next) => {
+  try{
+    res.render('job_seeking/intro');
+
+  }catch(err){
+    console.error(err);
+    next(err);
+  }
+});
+
+// 직업 설명 페이지
+router.get('/job_seeking/:title', async(req, res, next) => {
+  try{
+    let {title} = req.params;
+    console.log(title)
+    fs.readFile('public/asset/front.json', 'utf-8', (err, jsonFile) => {
+      // console.log(jsonFile)
+
+      jsonData = JSON.parse(jsonFile)
+      console.log(jsonData)
+
+      sendJson = jsonData[title]
+      res.render('job_seeking/job_desc', sendJson);
+
+    })
+
   }catch(err){
     console.error(err);
     next(err);
